3c69d7524e4a438beaa3ba353685b7b83bb72a2b
Ayushman Bilas Thakur <ayushmanbilasthakur@gmail.com>
2018-09-23 02:35:50 +0530

    Intial Commit

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const c = document.getElementById("C");
 const f = document.getElementById("F");
 const k = document.getElementById("K");
-const degc = "o"
 
 c.addEventListener("input",cinp);
 k.addEventListener("input",kinp);
@@ -9,29 +8,58 @@ f.addEventListener("input",finp)
 
 function cinp(e){
     cel = Number(c.value);
-    kel = cel + 273;
-    fer = 9 * cel / 5 +32;
-    k.value = kel;
-    f.value = fer;
+    
+    if(cel == ""){
+        empty_evrything();
+    }
+    else{
+        kel = cel + 273;
+        fer = 9 * cel / 5 +32;
+        set_everything(cel,fer,kel)
+    }
 }
 
 function kinp(e){
     kel = Number(k.value);
-    cel = kel - 273;
-    fer =  9 * cel / 5 +32;
+
+    if(kel == ""){
+        empty_evrything();
+    }
+
+    else{
+        cel = kel - 273;
+        fer =  9 * cel / 5 +32;
+        set_everything(cel,fer,kel)
+    }
+}
+
+function finp(e){
+    fer = Number(f.value);
+
+    if(fer == ""){
+        empty_evrything();
+    }
+
+    else{
+        cel = (fer -32)/9 * 5;
+        kel = cel + 273;
+        cel = (cel).toFixed(2);
+        kel = (kel).toFixed(2);
+        set_everything(cel,fer,kel)
+    }
+
     
+}
+
+empty_evrything = () => {
+    c.value = "";
+    f.value = "";
+    k.value = "";
+}
+
+set_everything = (cel,fer,kel) => {
     c.value = cel;
     k.value = kel;
     f.value = fer;
 }
 
-function finp(e){
-    fer = Number(f.value);
-    cel = (fer -32)/9 * 5;
-    kel = cel + 273;
-    cel = (cel).toFixed(2);
-    kel = (kel).toFixed(2);
-    c.value = cel;
-    k.value = kel;
-    f.value = fer;
-}
